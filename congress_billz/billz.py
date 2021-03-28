916045ec82377539c08666c1778862599f482769
maxmarzolf <max@treesfromspace.com>
2021-03-28 01:13:43 -0400

    Initial validation

--- congress_billz/billz.py
+++ congress_billz/billz.py
@@ -4,14 +4,14 @@ import requests
 
 
 api_key = os.environ.get('api_key')
+# get from api?
 valid_collections = ['USCOURTS', 'BILLS', 'CHRG', 'FR', 'GAOREPORTS', 'GOVPUB', 'CPD', 'CZIC', 'CFR', 'PLAW', 'CREC',
                      'CCAL', 'GPO', 'CDOC', 'CRECB', 'CPRT', 'PAI', 'USCODE', 'ERIC', 'BUDGET', 'ECONI', 'LSA', 'PPP',
                      'STATUTE', 'HOB', 'ERP', 'GOVMAN', 'CDIR', 'HMAN', 'HJOURNAL', 'SMAN']
 
 
 def get_date():
-    print('Enter date in YYYY-MM-DD format: ')
-    date = input()
+    date = input('Enter date in YYYY-MM-DD format: ')
     validate_date(date)
     return date
 
@@ -20,24 +20,28 @@ def validate_date(date):
     try:
         datetime.datetime.strptime(date, '%Y-%m-%d')
     except ValueError:
-        raise ValueError('Incorrect date format')
+        print(f'{date} invalid please enter date in YYYY-MM-DD format.')
+        get_date()
 
 
+# case insensitive
 def get_collection():
-    print('Enter collection: ')
-    collection = input()
-    if collection not in valid_collections:
-        print(f'{collection} is not valid, please choose a valid collection.')
+    user_collection = input('Enter collection: ')
+    if validate_collection(user_collection):
+        return user_collection
 
 
-# def validate_collection(collection):
-#     try:
-#         collection in valid_collections
+def validate_collection(user_collection):
+    if user_collection in valid_collections:
+        return True
+    else:
+        print(f'{user_collection} not valid, please choose a valid collection.')
+        get_collection()
 
 
-collection = input()
+collection = get_collection()
 start_date = get_date()
 end_date = get_date()
 url = f'https://api.govinfo.gov/published/{start_date}{end_date}?offset=0&pageSize=10&collection={collection}&api_key={api_key} '
-billz = requests.get()
+billz = requests.get(url)
 print(billz.text)
